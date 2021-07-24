1217645e5c18fab07fc903bbf453fc337da39dad
Sresh-creator <78287203+Sresh-creator@users.noreply.github.com>
2021-07-24 16:42:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Webcam.set({
+    width:300,
+    height:300,
+    image_format : 'png',
+    png_quality:90,
+
+    constraints: {
+        facingMode: "environment"
+    }
+});
+
+Webcam.attach('#camera');
+
+function take_snap(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+function check()
+{
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+console.error(error);
+     } else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
+classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+function modelLoaded(){
+    console.log('Model Loaded');
+}
